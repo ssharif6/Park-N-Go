39a998bc59b67d10c5e96434698f4041a2fd7240
Daniel Tran <qtof96@gmail.com>
2015-08-31 00:35:15 -0700

    Replaced GG Bridge with Favela

--- Park 'N Go/PinnedLocationTableViewCell.swift
+++ Park 'N Go/PinnedLocationTableViewCell.swift
@@ -14,6 +14,7 @@ class PinnedLocationTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
